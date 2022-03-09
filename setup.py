3ee02743ef24477abeec24432e8bee44cccad845
ArchieMeng <archiemeng@protonmail.com>
2022-03-09 13:13:10 +0800

    Update version to 1.1.2-3
    Use upstream 20220303 source archive for model download and fix
    rife-v2.3 and rife-v4 not found problems when installing from pypi
    
    Signed-off-by: ArchieMeng <archiemeng@protonmail.com>

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def download(url, chunk_size=4096):
 def download_models() -> None:
     rife_ncnn_vulkan_zip = download(
         "https://github.com/nihui/rife-ncnn-vulkan/archive/"
-        "c26c90bb643f93953204e3f3fcd1adc347336150.zip"
+        "c806e66490679aebc1b4a6832985e004fd552f46.zip"
     )
 
     with zipfile.ZipFile(rife_ncnn_vulkan_zip) as archive:
